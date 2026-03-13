32fa96d8ea74a49fab399e89f6229d30f7b12066
Sorsa55 <Laurilindgren03@outlook.com>
2026-03-13 18:54:52 +0200

    ToDoItem missing proper styling

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { ToDo } from '../types/Todo';
+
+interface Props {
+    todo: ToDo;
+    onToggle: () => void;
+    onDelete: () => void;
+}
+
+export default function ToDoItem({ todo, onToggle, onDelete }: Props) {
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress={onToggle}>
+                <Text style={[styles.text, todo.completed && styles.completed]}>
+                    {todo.title}
+                </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={onDelete}>
+                <Text style={styles.delete}>Delete</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 12,
+        borderBottomWidth: 1,
+    },
+    text: {
+        fontSize: 18,
+    },
+    completed: {
+        textDecorationLine: 'line-through',
+        color: 'gray',
+    },
+    delete: {
+        color: 'red',
+    },
+});
